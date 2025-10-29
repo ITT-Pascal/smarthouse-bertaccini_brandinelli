ba6e3c776bda16ceaaaa4520fbef1cf74f13eb43
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2025-10-29 20:05:37 +0100

    Created class Lamp and tested it

--- SmartHouse/src/SmartHouse.Domain/Lamp.cs
+++ SmartHouse/src/SmartHouse.Domain/Lamp.cs
@@ -3,6 +3,26 @@
     //Commit b
     public class Lamp
     {
-        public bool IsOn { get; private set; }
+        public bool IsOn => Brightness > 0;
+
+        public int Brightness { get; private set; }
+
+        const int MinBrightness = 0;
+        const int MaxBrightness = 100;
+
+        public Lamp()
+        {           
+            Brightness = 0;
+        }
+
+
+        public void ChangeBrightness(int newBrightness)
+        {
+            if(newBrightness >= MinBrightness)
+                Brightness = Math.Min(newBrightness, MaxBrightness);
+
+        }
+        
+
     }
 }
--- SmartHouse/test/SmartHouse.Domain.UnitTest/UnitTest1.cs
+++ SmartHouse/test/SmartHouse.Domain.UnitTest/UnitTest1.cs
@@ -1,12 +1,46 @@
 ﻿namespace SmartHouse.Domain.UnitTest
 {
     //Commit a
-    public class UnitTest1
+    public class LampTest
     {
-        [Fact]
-        public void Test1()
-        {
 
+        [Fact]
+        public void When_BrightnessIsNegative_BrightnessRemainsZero()
+        {
+            Lamp newLamp = new Lamp();
+
+            newLamp.ChangeBrightness(-1);
+
+            Assert.Equal(0, newLamp.Brightness);
+        }
+
+        [Fact]
+        public void When_BrightnessIsHigherThan100_BrightnessIs100()
+        {
+            Lamp newLamp = new Lamp();
+
+            newLamp.ChangeBrightness(101);
+
+            Assert.Equal(100, newLamp.Brightness);
+        }
+
+        [Fact]
+        public void When_BrightnessIs0_LampIsOff()
+        {
+            Lamp newLamp = new Lamp();
+
+            Assert.False(newLamp.IsOn);
+        }
+
+        [Fact]
+        public void When_BrightnessIs50_LampIsOn()
+        {
+            Lamp newLamp = new Lamp();
+
+            newLamp.ChangeBrightness(50);
+
+            Assert.True(newLamp.IsOn);
+            Assert.Equal(50, newLamp.Brightness);
         }
     }
 }
