2423e6a23792358a587710a4f403b32c8221b775
luca-bertaccini <luca.bertaccini.stud@ispascalcomandini.it>
2025-10-31 09:04:55 +0100

    Added EcoLampTest

---
+++ SmartHouse/test/SmartHouse.Domain.UnitTest/EcoLampTest.cs
@@ -0,0 +1,65 @@
+﻿namespace SmartHouse.Domain.UnitTest
+{
+    //Commit aaa
+    public class EcoLampTest
+    {
+
+        [Fact]
+        public void When_BrightnessIsNegative_BrightnessRemainsZero()
+        {
+            EcoLamp newEcoLamp = new EcoLamp();
+
+            newEcoLamp.ChangeBrightness(-1);
+
+            Assert.Equal(0, newEcoLamp.Brightness);
+        }
+
+        [Fact]
+        public void When_BrightnessIsHigherThan50_BrightnessIs50()
+        {
+            EcoLamp newEcoLamp = new EcoLamp();
+
+            newEcoLamp.SwitchOnOff();
+
+            newEcoLamp.ChangeBrightness(51);
+
+            Assert.Equal(50, newEcoLamp.Brightness);
+        }
+
+        [Fact]
+        public void When_LampIsOnButBrightnessIs0_LampIsOff()
+        {
+            EcoLamp newEcoLamp = new EcoLamp();
+
+            newEcoLamp.SwitchOnOff();
+
+            newEcoLamp.SwitchOnOff();
+
+            Assert.False(newEcoLamp.IsOn);
+        }
+
+        [Fact]
+        public void When_BrightnessIs25AndLampIsOn_LampIsOn()
+        {
+            EcoLamp newEcoLamp = new EcoLamp();
+
+            newEcoLamp.SwitchOnOff();
+
+            newEcoLamp.ChangeBrightness(25);
+
+            Assert.True(newEcoLamp.IsOn);
+            Assert.Equal(25, newEcoLamp.Brightness);
+        }
+
+        [Fact]
+        public void When_BrightnessIs25ButLampIsOff_LampIsOff()
+        {
+            EcoLamp newEcoLamp = new EcoLamp();
+
+            newEcoLamp.ChangeBrightness(25);
+
+            Assert.False(newEcoLamp.IsOn);
+        }
+    }
+
+}
