a3e76b0ac9fc3077f309b06ba7945350fd1a4648
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2025-11-07 08:58:33 +0100

    Added LampsRow class to manage an n number of Lamps and EcoLamps from a single point

--- SmartHouse/src/SmartHouse.Domain/LampsRow.cs
+++ SmartHouse/src/SmartHouse.Domain/LampsRow.cs
@@ -8,7 +8,55 @@ namespace SmartHouse.Domain
 {
     public class LampsRow
     {
-               
+        public List<AbstractLamp> Lamps { get; private set; }
+
+        public LampsRow()
+        {
+            Lamps = new List<AbstractLamp>();
+        }
+
+        public void AddEcoLamp()
+        {           
+             Lamps.Add(new EcoLamp());           
+        }
+
+        public void AddLamp()
+        {         
+             Lamps.Add(new Lamp());          
+        }
+
+        public void SingleLampSwitchOnOff(int selectedLamp)
+        {
+            if (selectedLamp < 0 || selectedLamp >= Lamps.Count)
+                throw new ArgumentException("Selected lamp doesn't exist");
+
+            Lamps[selectedLamp].SwitchOnOff();
+        }
+
+        public void SingleLampChangeBrightness(int newBrightness, int selectedLamp)
+        {
+            if (selectedLamp < 0 || selectedLamp >= Lamps.Count)
+                throw new ArgumentException("Selected lamp doesn't exist");
+
+            Lamps[selectedLamp].ChangeBrightness(newBrightness);
+        }
+
+        public void AllLampsSwitchOnOff()
+        {
+            for(int i = 0; i < Lamps.Count; i++)
+            {
+                Lamps[i].SwitchOnOff();
+            }
+
+        }
+
+       public void AllLampsChangeBrightness(int newBrightness)
+        {
+            for (int i = 0; i < Lamps.Count; i++)
+            {               
+                Lamps[i].ChangeBrightness(newBrightness);
+            }
+        }
 
 
 
