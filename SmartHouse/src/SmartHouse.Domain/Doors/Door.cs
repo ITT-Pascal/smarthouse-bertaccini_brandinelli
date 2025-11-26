ce608bb98f1663cb290aa27e53b5bd325c2f4185
luca-bertaccini <luca.bertaccini.stud@ispascalcomandini.it>
2025-11-26 16:01:55 +0100

    Added Door class

---
+++ SmartHouse/src/SmartHouse.Domain/Doors/Door.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.Domain.Doors
+{
+    public class Door
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DoorStatus Status { get; set; }
+
+        public Door(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Status = DoorStatus.Closed;
+        }
+
+        public void OpenDoor()
+        {
+            if (Status == DoorStatus.Closed)
+                Status = DoorStatus.Open;
+        }
+
+        public void CloseDoor()
+        {
+            if (Status == DoorStatus.Open)
+                Status = DoorStatus.Closed;
+        }
+
+        public void LockDoor()
+        {
+            if (Status == DoorStatus.Closed)
+                Status = DoorStatus.Locked;
+
+        }
+
+        public void UnlockDoor()
+        {
+            if (Status == DoorStatus.Locked)
+                Status = DoorStatus.Closed;
+        }
+
+    }
+}
---
+++ SmartHouse/src/SmartHouse.Domain/Doors/DoorStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.Domain.Doors
+{
+    public enum DoorStatus
+    {
+        Open = 0,
+        Closed = 1,
+        Locked = 2,
+    }
+}
