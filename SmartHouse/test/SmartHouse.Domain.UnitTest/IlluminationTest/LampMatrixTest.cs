9214dcbf94716d41841cf5700b6d1f4d13ea8640
luca-bertaccini <luca.bertaccini.stud@ispascalcomandini.it>
2025-12-10 10:59:41 +0100

    Added LampMatrixTest

---
+++ SmartHouse/test/SmartHouse.Domain.UnitTest/IlluminationTest/LampMatrixTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SmartHouse.Domain.IlluminationDevice;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHouse.Domain.Illumination;
+
+namespace SmartHouse.Domain.UnitTest.IlluminationTest
+{
+    public class LampMatrixTest
+    {
+        [Fact]
+        public void When_WantToAddANewLamp_CanDoIt()
+        {
+            AbstractLamp[,] lamps = new AbstractLamp[3, 3];
+            Lamp newLamp = new Lamp("Stefano");
+            LampMatrix newLampMatrix = new LampMatrix("Sasha", lamps);
+
+            newLampMatrix.AddLamp(newLamp);
+
+            
+        }
+    }
+}
