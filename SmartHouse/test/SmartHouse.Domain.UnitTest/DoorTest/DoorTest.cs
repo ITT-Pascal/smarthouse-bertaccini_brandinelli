69e18507f207480f51c16edc3a0016f3ffab4eab
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2026-01-23 18:00:05 +0100

    fixed door test

--- SmartHouse/test/SmartHouse.Domain.UnitTest/DoorTest/DoorTest.cs
+++ SmartHouse/test/SmartHouse.Domain.UnitTest/DoorTest/DoorTest.cs
@@ -12,53 +12,51 @@ namespace SmartHouse.Domain.UnitTest.DoorTest
 {  
     public class DoorTest
     {
-        private Pin PIN = new Pin(1234);
-        private Pin PIN2 = new Pin(00);
-        private Pin PIN3 = new Pin(1230);
-        
+
+        Pin PIN = Pin.Create(1234);
+        Pin PIN2 = Pin.Create(1230);
 
         [Fact]
 
         public void When_GivenANullStringAsAName_ThrowArgumentException()
         {
-            Assert.Throws<ArgumentException>(() => new Door(null, PIN));
+            Assert.Throws<ArgumentException>(() => new Door(null, 1234));
         }
 
         [Fact]
         public void When_TheNameOfTheDoorIsEmpty_TheNameIsNotValid()
         {
-            Assert.Throws<ArgumentException>(() => new Door(new Name(string.Empty), PIN));
+            Assert.Throws<ArgumentException>(() => new Door(string.Empty, 1234));
         }
 
         [Fact]
 
         public void When_GivenANameThatIsOnlySpaces_ThrowArgumentException()
         {
-            Assert.Throws<ArgumentException>(() => new Door(new Name("    "), PIN));
+            Assert.Throws<ArgumentException>(() => new Door("    ", 1234));
         }
 
         [Fact]
         public void When_ThePINOfTheDoorIsUnder4Digits_ThePINIsNotValid()
         {
-            Assert.Throws<ArgumentException>(() => new Door(new Name("Giovanni"), PIN2));
+            Assert.Throws<ArgumentException>(() => new Door("Giovanni", 00));
         }
 
         [Fact]
         public void When_TheDoorIsClosed_CanOpenIt()
         {
-            Door newDoor = new Door(new Name("Giovanni"), PIN);
+            Door newDoor = new Door("Giovanni", 1234);
 
             newDoor.Unlock(PIN);
             newDoor.Open();
 
             Assert.True(newDoor.IsOpen);
-            Assert.Equal(DeviceStatus.Unknown, newDoor.Status);
         }
 
         [Fact]
         public void When_TheDoorIsLocked_CannotOpenIt()
         {
-            Door newDoor = new Door(new Name("Giovanni"), PIN);
+            Door newDoor = new Door("Giovanni", 1234);
 
             newDoor.Unlock(PIN);
             newDoor.Lock();
@@ -66,80 +64,73 @@ namespace SmartHouse.Domain.UnitTest.DoorTest
             Assert.Throws<ArgumentException>(() => newDoor.Open());
             Assert.False(newDoor.IsOpen);
             Assert.True(newDoor.IsLocked);
-            Assert.Equal(DeviceStatus.Unknown, newDoor.Status);
         }
 
         [Fact]
         public void When_TheDoorIsOpen_CanCloseIt()
         {
-            Door newDoor = new Door(new Name("Giovanni"), PIN);
+            Door newDoor = new Door("Giovanni", 1234);
 
             newDoor.Unlock(PIN);
             newDoor.Open();
             newDoor.Close();
 
             Assert.False(newDoor.IsOpen);
-            Assert.Equal(DeviceStatus.Unknown, newDoor.Status);
         }
 
         [Fact]
         public void When_TheDoorIsOpen_CannotLockIt()
         {
-            Door newDoor = new Door(new Name("Giovanni"), PIN);
+            Door newDoor = new Door("Giovanni", 1234);
 
             newDoor.Unlock(PIN);
             newDoor.Open();
 
             Assert.Throws<ArgumentException>(() => newDoor.Lock());
             Assert.True(newDoor.IsOpen);
-            Assert.Equal(DeviceStatus.Unknown, newDoor.Status);
         }
 
         [Fact]
         public void When_TheDoorIsClosed_CanLockIt()
         {
-            Door newDoor = new Door(new Name("Giovanni"), PIN);
+            Door newDoor = new Door("Giovanni", 1234);
 
             newDoor.Unlock(PIN);
             newDoor.Lock();
 
             Assert.False(newDoor.IsOpen);
             Assert.True(newDoor.IsLocked);
-            Assert.Equal(DeviceStatus.Unknown, newDoor.Status);
         }
 
         [Fact]
         public void When_TheDoorIsOpen_CannotUnlockIt()
         {
-            Door newDoor = new Door(new Name("Giovanni"), PIN);
+            Door newDoor = new Door("Giovanni", 1234);
 
             newDoor.Unlock(PIN);
             newDoor.Open();
 
             Assert.Throws<ArgumentException>(() => newDoor.Unlock(PIN));
             Assert.True(newDoor.IsOpen);
-            Assert.Equal(DeviceStatus.Unknown, newDoor.Status);
         }
 
         [Fact]
         public void When_TheDoorIsLockedAndThePINIsWrong_CannotUnlockIt()
         {
-            Door newDoor = new Door(new Name("Giovanni"), PIN);
+            Door newDoor = new Door("Giovanni", 1234);
 
-            Assert.Throws<ArgumentException>(() => newDoor.Unlock(PIN3));
+            Assert.Throws<ArgumentException>(() => newDoor.Unlock(PIN2));
             Assert.True(newDoor.IsLocked);
-            Assert.Equal(DeviceStatus.Unknown, newDoor.Status);
         }
 
         [Fact]
         public void When_TheDoorIsLockedAndThePINIsCorrect_CanUnlockIt()
         {
-            Door newDoor = new Door(new Name("Giovanni"), PIN);
+            Door newDoor = new Door("Giovanni", 1234);
 
             newDoor.Unlock(PIN);
 
             Assert.False(newDoor.IsOpen);
-            Assert.Equal(DeviceStatus.Unknown, newDoor.Status);
         }
     }
 }
