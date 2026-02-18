c00b26386810a11f17c314dc35ba7c6d1257e838
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2026-02-18 10:59:18 +0100

    Added Command On Lamp In Application Layer

---
+++ SmartHouse/src/SmartHouse.Application/Devices/Illumination/Lamps/Commands/ChangeBrightnessLampCommand.cs
@@ -0,0 +1,31 @@
+﻿using SmartHouse.Domain.Illumination;
+using SmartHouse.Domain.IlluminationDevice;
+using SmartHouse.Domain.IlluminationDevice.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.Application.Devices.Illumination.Lamps.Commands
+{
+    public class ChangeBrightnessLampCommand
+    {
+        private ILampRepository _lampRepository;
+
+        public ChangeBrightnessLampCommand(ILampRepository lampRepository)
+        {
+            _lampRepository = lampRepository;
+        }
+
+        public void Execute(Guid id, int newbrightness)
+        {
+            Lamp lamp = _lampRepository.GetById(id);
+            if(lamp != null)
+            {
+                lamp.ChangeBrightness(newbrightness);
+                _lampRepository.Update(lamp);
+            }
+        }
+    }
+}
