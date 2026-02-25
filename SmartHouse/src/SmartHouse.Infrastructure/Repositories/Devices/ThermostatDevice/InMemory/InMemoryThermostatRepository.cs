5a0ad3c9859a4462bb3d04057b8e1c86054b642a
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2026-02-25 11:01:20 +0100

    Added InMemoryThermostatRepository

---
+++ SmartHouse/src/SmartHouse.Infrastructure/Repositories/Devices/ThermostatDevice/InMemory/InMemoryThermostatRepository.cs
@@ -0,0 +1,62 @@
+﻿using SmartHouse.Domain.Illumination;
+using SmartHouse.Domain.TemperatureDevice;
+using SmartHouse.Domain.TemperatureDevice.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.Infrastructure.Repositories.Devices.ThermostatDevice.InMemory
+{
+    public class InMemoryThermostatRepository : IThermostatRepository
+    {
+        private readonly List<Thermostat> _thermostats;
+
+        public InMemoryThermostatRepository()
+        {
+            _thermostats = new List<Thermostat>
+            {
+                new Thermostat("Franco"),
+                new Thermostat("Matteo"),
+                new Thermostat("Giacomo")
+            };
+        }
+
+        public List<Thermostat> GetAll()
+        {
+            return _thermostats;
+        }
+
+        public Thermostat? GetById(Guid id)
+        {
+            Thermostat? result = null;
+            foreach (Thermostat t in _thermostats)
+                if (t.Id == id)
+                    result = t;
+
+            return result;
+        }
+
+        public void Add(Thermostat t)
+        {
+            if (t != null)
+                _thermostats.Add(t);
+            else
+                throw new ArgumentException("Thermostat cannot be null");
+        }
+
+        public void Delete(Guid id)
+        {
+            var thermostat = GetById(id);
+
+            if (thermostat != null)
+                _thermostats.Remove(thermostat);
+        }
+
+        public void Update(Thermostat t)
+        {
+            //To do
+        }
+    }
+}
