a239b29e4d5ac3cf7c64b22d8001f19dc8a4228c
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2026-02-27 09:47:25 +0100

    Added Console Layer

---
+++ SmartHouse/src/SmartHouse.Console/Devices/Illumination/Lamps/Controllers/LampController.cs
@@ -0,0 +1,48 @@
+﻿using SmartHouse.Application.Devices.Illumination.Lamps.Commands;
+using SmartHouse.Application.Devices.Illumination.Lamps.Queries;
+using SmartHouse.Domain.IlluminationDevice.Repositories;
+
+public class LampController
+{
+    private readonly ILampRepository _repository;
+
+    public LampController(ILampRepository repos)
+    {
+        _repository = repos;
+    }
+
+    public void AddLamp()
+    {
+        Console.Write("Lamp name: ");
+        string name = Console.ReadLine();
+
+        if(string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Invalid name");
+            return;
+        }
+
+        new AddLampCommand(_repository).Execute(name);
+        Console.WriteLine("Lamp added!");
+    }
+
+    public void ShowLamps()
+    {
+        var lamps = new GetAllLampsQuery(_repository).Execute();
+
+        Console.WriteLine("LAMPS:");
+        Console.WriteLine("------------------------------");
+
+        if(lamps.Count == 0)
+        {
+            Console.WriteLine("No lamps available");
+            return;
+        }
+
+        for(int i = 0; i < lamps.Count;i++)
+        {
+            var l = lamps[i];
+            Console.WriteLine($"{i + 1}. {l.Name}\n{l}");
+        }
+    }
+}
