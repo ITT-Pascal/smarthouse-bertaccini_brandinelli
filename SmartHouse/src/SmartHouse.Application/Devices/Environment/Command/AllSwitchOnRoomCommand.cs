1732e7b68a8be3c21fc16d5e29154bed4d6e0c13
luca-bertaccini <luca.bertaccini.stud@ispascalcomandini.it>
2026-03-02 12:37:49 +0100

    Added something in Room in Applicition layer

---
+++ SmartHouse/src/SmartHouse.Application/Devices/Environment/Command/AllSwitchOnRoomCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHouse.Domain.Environment.Repositories;
+
+namespace SmartHouse.Application.Devices.Environment.Command
+{
+    public class AllSwitchOnRoomCommand
+    {
+        private readonly IRoomRepository _roomRepository;
+
+        public AllSwitchOnRoomCommand(IRoomRepository roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
+    }
+}
