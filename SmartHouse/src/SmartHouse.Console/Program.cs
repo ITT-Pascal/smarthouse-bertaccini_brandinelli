a1b363951a43b0c6ae0b6259c3de3663b07e51da
luca-bertaccini <luca.bertaccini.stud@ispascalcomandini.it>
2026-03-11 14:51:17 +0100

    Added interface with arrows keys in Program

--- SmartHouse/src/SmartHouse.Console/Program.cs
+++ SmartHouse/src/SmartHouse.Console/Program.cs
@@ -20,56 +20,87 @@ class Program
 {
     static void Main()
     {
-
         bool exit = false;
+
+        string[] options = { "0 - Exit", "1 - Lamps", "2 - CCTVS", "3 - Air Conditioner", "4 - Thermostat", "5 - Door" };
+        int selected = 0;
+
+        Console.CursorVisible = false;
 
         while (!exit)
         {
-            Console.Clear();
-            Console.Write("\x1b[3J");
+            bool choiceDone = false;
 
-            Console.WriteLine("0 - Exit \n" +
-                          "1 - Lamps \n" +
-                          "2 - CCTVS \n" +
-                          "3 - Air Conditioner \n" +
-                          "4 - Thermostat \n" +
-                          "5 - Door \n");
-
-            Console.Write("Choose an option: ");
-            string choice = Console.ReadLine();
-
-            switch (choice)
+            while (!choiceDone)
             {
-                case "0":
+                Console.Clear();
+                Console.Write("\x1b[3J");
+
+                Console.WriteLine("--- SMART HOUSE SYSTEM ---");
+                Console.WriteLine("(Use the arrows keys to move, Enter to select)\n");
+
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (i == selected)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine($"> {options[i]} ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"  {options[i]} ");
+                    }
+                }
+
+                Console.WriteLine("--------------------------");
+
+                ConsoleKey info = Console.ReadKey(true).Key;
+
+                switch (info)
+                {
+                    case ConsoleKey.UpArrow:
+                        selected = (selected == 0) ? options.Length - 1 : selected - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selected = (selected == options.Length - 1) ? 0 : selected + 1;
+                        break;
+                    case ConsoleKey.Enter:
+                        choiceDone = true;
+                        break;
+                }
+            }
+
+            switch (selected)
+            {
+                case 0:
                     exit = true;
                     break;
-                case "1":
+                case 1:
                     ILampRepository lampRepository = new InMemoryLampRepository();
                     LampController lampController = new LampController(lampRepository);
                     lampController.ShowMenu(lampController);
                     break;
-                case "2":
+                case 2:
                     ICCTVRepository cctvRepository = new InMemoryCCTVRepository();
                     CCTVController cctvController = new CCTVController(cctvRepository);
                     cctvController.ShowMenu(cctvController);
                     break;
-                case "3":
+                case 3:
                     IAirConditionerRepository airConditionerRepository = new InMemoryAirConditionerRepository();
                     AirConditionerController airConditionerController = new AirConditionerController(airConditionerRepository);
                     airConditionerController.ShowMenu(airConditionerController);
                     break;
-                case "4":
+                case 4:
                     IThermostatRepository thermostatRepository = new InMemoryThermostatRepository();
                     ThermostatController thermostatController = new ThermostatController(thermostatRepository);
                     thermostatController.ShowMenu(thermostatController);
                     break;
-                case "5":
+                case 5:
                     IDoorRepository doorRepository = new InMemoryDoorRepository();
                     DoorController doorController = new DoorController(doorRepository);
                     doorController.ShowMenu(doorController);
-                    break; 
-                default:
-                    Console.WriteLine("Invalid Choice");
                     break;
             }
         }
@@ -78,6 +109,6 @@ class Program
         Console.Write("\x1b[3J");
         Console.WriteLine("See you next time!");
         Console.Write("\nPress enter to close the menu....");
-        Console.Write("\n", 10);
+        Console.ReadLine();
     }
 }
